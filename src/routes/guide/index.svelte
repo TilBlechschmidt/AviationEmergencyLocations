2552fc49632880d6ff2667d0bd43bd5ee1d4bf13
Til Blechschmidt <til@blechschmidt.de>
2021-12-12 23:42:29 +0100

    :globe_with_meridians: Internationalize the guide intro

--- src/routes/guide/index.svelte
+++ src/routes/guide/index.svelte
@@ -1,3 +1,7 @@
+<script>
+	import { _ } from 'svelte-i18n';
+</script>
+
 <img
 	src="/guide/learning.gif"
 	alt="Bad joke, I don't even like Star Wars 🤷‍♂️"
@@ -5,16 +9,12 @@
 	class="rounded-xl w-full mb-8"
 />
 
-Welcome to the E.L.S.A. Handbook! This guide encompasses a multitude of topics for you to read. It
-starts off with an introduction into the base-concepts applied within all the tools, then it gives
-detailed explanations of every tool provided with everything you should know to interpret the data
-correctly. Lastly, we have compiled information from a number of external sources which might be
-helpful in an emergency situation.
-<br />
-<br />
-We hope that this compendium serves you well! If you have any questions, feedback on how we can do even
-better, or ideas for new features feel free to reach out using the contact information in the
-<a href="/imprint">imprint</a>.
-<br />
-<br />
-Thank you for using E.L.S.A., we hope to see you in the sky!
+{@html $_('guide.overview.preface')}
+
+<ul class="p-4 list-disc list-inside flex flex-col space-y-2">
+	<li>{$_('guide.overview.sections.introduction')}</li>
+	<li>{$_('guide.overview.sections.tools')}</li>
+	<li>{$_('guide.overview.sections.knowledge')}</li>
+</ul>
+
+{@html $_('guide.overview.conclusion')}
